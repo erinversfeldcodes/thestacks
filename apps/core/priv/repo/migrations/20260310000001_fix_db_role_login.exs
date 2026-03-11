983734b01e0382fcbc5fc2dc691b25d30fb53eb0
Erin Versfeld <erinversfeld@gmail.com>
2026-03-11 10:53:19 +0200

    fix: make db role migrations resilient for hosted postgres

--- apps/core/priv/repo/migrations/20260310000001_fix_db_role_login.exs
+++ apps/core/priv/repo/migrations/20260310000001_fix_db_role_login.exs
@@ -11,25 +11,40 @@ defmodule Core.Repo.Migrations.FixDbRoleLogin do
   use Ecto.Migration
 
   def up do
-    execute("ALTER ROLE stacks_app LOGIN PASSWORD 'stacks_app'")
-    execute("ALTER ROLE stacks_dbt LOGIN PASSWORD 'stacks_dbt'")
-    # stacks_dbt needs SELECT on audit schema to build stg_audit_log
-    execute("GRANT USAGE ON SCHEMA audit TO stacks_dbt")
-    execute("GRANT SELECT ON ALL TABLES IN SCHEMA audit TO stacks_dbt")
-    # GRANT CONNECT + CREATE on the current database so:
-    #   - stacks_app / stacks_dbt can connect (CONNECT)
-    #   - stacks_dbt can create schemas (e.g. wh_staging) at dbt run time (CREATE)
-    # Uses current_database() so this works for any db name (stacks_dev, stacks_test, etc.)
+    app_password = System.get_env("STACKS_APP_DB_PASSWORD")
+    dbt_password = System.get_env("STACKS_DBT_DB_PASSWORD")
+
     execute("""
     DO $$ BEGIN
-      EXECUTE format('GRANT CONNECT ON DATABASE %I TO stacks_app, stacks_dbt, stacks_readonly', current_database());
-      EXECUTE format('GRANT CREATE ON DATABASE %I TO stacks_dbt', current_database());
+      IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_app') THEN
+        #{if app_password, do: "ALTER ROLE stacks_app LOGIN PASSWORD '#{app_password}';", else: "ALTER ROLE stacks_app LOGIN;"}
+      END IF;
+      IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_dbt') THEN
+        #{if dbt_password, do: "ALTER ROLE stacks_dbt LOGIN PASSWORD '#{dbt_password}';", else: "ALTER ROLE stacks_dbt LOGIN;"}
+        GRANT USAGE ON SCHEMA audit TO stacks_dbt;
+        GRANT SELECT ON ALL TABLES IN SCHEMA audit TO stacks_dbt;
+      END IF;
+      IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_app')
+        OR EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_dbt') THEN
+        EXECUTE format('GRANT CONNECT ON DATABASE %I TO stacks_app, stacks_dbt, stacks_readonly', current_database());
+      END IF;
+      IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_dbt') THEN
+        EXECUTE format('GRANT CREATE ON DATABASE %I TO stacks_dbt', current_database());
+      END IF;
     END $$;
     """)
   end
 
   def down do
-    execute("ALTER ROLE stacks_app NOLOGIN")
-    execute("ALTER ROLE stacks_dbt NOLOGIN")
+    execute("""
+    DO $$ BEGIN
+      IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_app') THEN
+        ALTER ROLE stacks_app NOLOGIN;
+      END IF;
+      IF EXISTS (SELECT FROM pg_roles WHERE rolname = 'stacks_dbt') THEN
+        ALTER ROLE stacks_dbt NOLOGIN;
+      END IF;
+    END $$;
+    """)
   end
 end
