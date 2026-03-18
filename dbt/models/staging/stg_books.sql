eee2bf5f00fb17fbf94d6706f7f8d4d98183f324
Erin Versfeld <erinversfeld@gmail.com>
2026-03-18 14:23:45 +0200

    feat: add stg_book_editions staging model, update stg_books

--- dbt/models/staging/stg_books.sql
+++ dbt/models/staging/stg_books.sql
@@ -4,20 +4,13 @@
 
 select
     id,
-    isbn,
     title,
     author_id,
     description,
-    cover_image_url,
-    page_count,
-    publisher,
-    publication_year,
     language,
     subjects,
     bisac_codes,
     visibility_tier,
-    open_library_id,
-    google_books_id,
     created_at,
     updated_at
 from {{ source('op', 'books') }}
