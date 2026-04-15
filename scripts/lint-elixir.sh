fe8900068ac5cac346beb4145fc10dbe17926807
Erin Versfeld <erinversfeld@gmail.com>
2026-04-15 13:47:37 +0200

    fix: use correct conifg

--- scripts/lint-elixir.sh
+++ scripts/lint-elixir.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 mix format --check-formatted
 mix credo --strict
 mix dialyzer
-(cd apps/core && mix sobelow --config --ignore-files test/support/)
+(cd apps/core && mix sobelow --config)
 mix deps.audit
